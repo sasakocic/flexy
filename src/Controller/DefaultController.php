d9709b51138b6a60d84dc7af1ef027d4d428d869
Sasa Kocic <sasa.kocic@yogaeasy.de>
2021-03-02 10:13:22 +0100

    composer require sensio/framework-extra-bundle

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,14 +2,26 @@
 
 namespace App\Controller;
 
-//use Symfony\Bundle\FrameworkBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     public function index()
     {
         return $this->render('default/index.html.twig', ['greeting' => 'hello']);
+    }
+
+    /**
+     * @Route("/api/hello/{name}")
+     * @param string $name
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function apiExample(string $name)
+    {
+        return $this->json([
+            'name' => $name,
+            'symfony' => 'rocks',
+        ]);
     }
 }
