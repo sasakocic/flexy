4d0afca440fb7ba49575988bb2c701b2a607fe10
Sasa Kocic <sasa.kocic@gmail.com>
2021-03-27 17:48:14 +0100

    add test

---
+++ tests/live/ExampleLiveTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleLiveTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        static::assertEquals(1, 1);
+    }
+}
