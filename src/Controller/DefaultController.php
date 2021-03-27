34d3531d4a7d7bd309eb5fe7dd85f720b2b3f2b3
Sasa Kocic <sasa.kocic@gmail.com>
2021-03-27 15:10:01 +0100

    update

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,27 +1,1 @@
-<?php
-
-namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-
-class DefaultController extends AbstractController
-{
-    public function index()
-    {
-        return $this->render('default/index.html.twig', ['greeting' => 'hello']);
-    }
-
-    /**
-     * @Route("/api/hello/{name}")
-     * @param string $name
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
-     */
-    public function apiExample(string $name)
-    {
-        return $this->json([
-            'name' => $name,
-            'symfony' => 'rocks',
-        ]);
-    }
-}
+<?phpnamespace App\Controller;use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;use Symfony\Component\Routing\Annotation\Route;class DefaultController extends AbstractController{    public function index()    {        return $this->render('default/index.html.twig', ['greeting' => 'hello']);    }    /**     * @Route("/api/hello/{name}")     * @param string $name     * @return \Symfony\Component\HttpFoundation\JsonResponse     */    public function apiExample(string $name)    {        return $this->json([            'name' => $name,            'symfony' => 'rocks',        ]);    }}
